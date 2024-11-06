7af40f5e96d40d3d1ae330fc8fff273836d2ebcf
Katerina <76427845+Katerinajames@users.noreply.github.com>
2024-11-06 19:12:29 +0200

    Add files via upload

---
+++ gcd2.py
@@ -0,0 +1,11 @@
+import math
+		
+		
+    
+    
+print("------------------------------------------------------------------")    
+
+number=int(input("Enter the first integer\n"))
+number2=int(input("Enter the second integer \n"))
+
+print("The greatest common divisor of the integers you have just entered is %d"%(math.gcd(number,number2)))
